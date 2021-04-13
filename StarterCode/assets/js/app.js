0feb084cb01d933b6d496eb611efa9c7a4c2ee17
Lauren Parrish <ljeanparrish@gmail.com>
2021-04-13 10:10:48 -0400

    updates to js code and html

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,6 +1,6 @@
 // Set up initial parameters for svg image
 var svgWidth = 960;
-var svgHeight = 500;
+var svgHeight = 700;
 
 var margin = {
     top: 20,
@@ -13,7 +13,7 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
+var svg = d3.select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
@@ -38,7 +38,7 @@ d3.csv("assets/data/data.csv").then(function (healthData) {
     // Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-        .domain([20, d3.max(healthData, d => d.poverty)])
+        .domain([0, d3.max(healthData, d => d.poverty)])
         .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
@@ -50,7 +50,7 @@ d3.csv("assets/data/data.csv").then(function (healthData) {
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
-    // Step 4: Append Axes to the chart
+    // Append Axes to the chart
     // ==============================
     chartGroup.append("g")
         .attr("transform", `translate(0, ${height})`)
@@ -102,12 +102,17 @@ d3.csv("assets/data/data.csv").then(function (healthData) {
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Percentage of Poverty");
+      .style("fill", "black")
+      .style("font", "20px sans-serif")
+      .style("font-weight", "bold")
+      .text("People without Healthcare (%)");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text("Lacks Healthcare %");
+      .style("font", "20px sans-serif")
+      .style("font-weight", "bold")
+      .text("Poverty (%)");
       
   }).catch(function(error) {
     console.log(error);
